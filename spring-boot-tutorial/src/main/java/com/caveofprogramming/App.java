5c940fe973e351a41eaa7e6e8d0c4f133ecd06c3
j4sysiak <j4sysiak@gmail.com>
2018-12-01 06:05:32 +0100

    3

--- spring-boot-tutorial/src/main/java/com/caveofprogramming/App.java
+++ spring-boot-tutorial/src/main/java/com/caveofprogramming/App.java
@@ -2,23 +2,26 @@ package com.caveofprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
 
+@Controller
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
-	
-	
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(App.class);
-    }
-	
+	  public static void main(String[] args) throws Exception {
+		    SpringApplication.run(App.class, args);
+		  }
 
+		  @RequestMapping("/")
+		  String index() {
+		    return "index";
+		  }
+		  
+		  @RequestMapping("/admin")
+		  String admin() {
+		    return "admin";
+		  }
 
 }
